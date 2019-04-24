f253efa640348212abc2b1f2b765464914333b88
mmyenny <mmy491@gmail.com>
2019-04-24 00:06:01 -0400

    Added range type input elements for HSL
    Added CSS

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Color Picker</h1>
+      <div className="container">
+        <div className="box" />
+        <div className="sliders">
+          <ul>
+            <li>
+              <input type="range" min="0" max="360" />
+              <label>Hue</label>
+            </li>
+            <li>
+              <input type="range" min="0" max="100" />
+              <label>Saturation</label>
+            </li>
+            <li>
+              <input type="range" min="0" max="100" />
+              <label>Lightness</label>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
