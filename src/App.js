ce549ff857b9a086096ca39870f6155baa755a66
mmyenny <mmy491@gmail.com>
2019-04-24 23:14:07 -0400

    Created static state

--- src/App.js
+++ src/App.js
@@ -1,31 +1,53 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Color Picker</h1>
-      <div className="container">
-        <div className="box" />
-        <div className="sliders">
-          <ul>
-            <li>
-              <input type="range" min="0" max="360" />
-              <label>Hue</label>
-            </li>
-            <li>
-              <input type="range" min="0" max="100" />
-              <label>Saturation</label>
-            </li>
-            <li>
-              <input type="range" min="0" max="100" />
-              <label>Lightness</label>
-            </li>
-          </ul>
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      hue: 0,
+      saturation: 0,
+      lightness: 0
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Color Picker</h1>
+        <div className="container">
+          <div className="box" />
+          <div className="sliders">
+            <ul>
+              <li>
+                <input type="range" value={this.state.hue} min="0" max="360" />
+                <label>Hue</label>
+              </li>
+              <li>
+                <input
+                  type="range"
+                  value={this.state.saturation}
+                  min="0"
+                  max="100"
+                />
+                <label>Saturation</label>
+              </li>
+              <li>
+                <input
+                  type="range"
+                  value={this.state.lightness}
+                  min="0"
+                  max="100"
+                />
+                <label>Lightness</label>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default App
